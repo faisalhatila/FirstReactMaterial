30a208640543e50101ed47111c712e27d335b3d2
Faisal <hatilas1994@gmail.com>
2019-07-28 05:38:57 -0700

    Added an <Add/> icon to create new exercise

--- src/Components/Exercises/Dialogues/Create.jsx
+++ src/Components/Exercises/Dialogues/Create.jsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
 
-export default props => {
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default class Create extends Component {
+    state = {
+        open : false,
+    }
     
+    handleToggle = () => {
+        this.setState({
+            open : !this.state.open
+        })
+    }
+    render() {
+        const {open} = this.state;
+        return (
+            <Fragment>
+                <Fab size="small" onClick={this.handleToggle} aria-label="add">
+                    <AddIcon />
+                </Fab>
+                <Dialog
+                open={open}
+                onClose = {this.handleToggle}
+                >
+                    <DialogTitle id="form-dialog-title">
+                        Create a New Exercise
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please Fill the form
+                        </DialogContentText>
+                        <form>
+
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button color="primary" variant="contained">
+                            Create
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Fragment>
+        )
+    }
 }
--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+import Create from '../Exercises/Dialogues/Create';
 // import Toolbar from '@material-ui/core/Toolbar';
 // import Typography from '@material-ui/core/Typography';
 // import Button from '@material-ui/core/Button';
@@ -11,9 +12,11 @@ export default props => {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h5" gutterBottom color="initial">
+                <Typography variant="h5" gutterBottom color="initial" style={{flex : 1}}>
                     Exercise DataBase
                 </Typography>
+
+                <Create color='primary'/>
             </Toolbar>
         </AppBar>
     )
